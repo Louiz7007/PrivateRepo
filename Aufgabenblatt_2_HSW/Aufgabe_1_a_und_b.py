ecea5a92ac75ddd69eed9009107c537035ddeab1
louiz7007 <louis.dittmann@web.de>
2021-10-21 17:42:23 +0200

    Aufgabe 1 - Python

--- Aufgabenblatt_2_HSW/Aufgabe_1_a_und_b.py
+++ Aufgabenblatt_2_HSW/Aufgabe_1_a_und_b.py
@@ -1,6 +1,12 @@
+# Aufgabe 1: Eingabe getankte Benzinmenge, Eingabe Brutto Preis pro Liter
+# Ausgabe Getankte Liter, Ausgabe Preis pro Liter, Ausgabe Gesamtpreis Brutto, Ausgabe Gesamtpreis Netto
+
 menge = input("Getankte Benzinmenge in Litern: ");
 bruttoPreisLiter = input("Preis je Liter (EUR): ");
 bruttoPreisGesamt = float(menge) * float(bruttoPreisLiter);
 print("Getankte Liter: ", menge);
 print("Preis pro Liter: ", bruttoPreisLiter);
-print("Gesamtbetrag Tankfüllung: ", bruttoPreisGesamt);
+print("Gesamtbetrag Tankfüllung (Brutto): ", bruttoPreisGesamt);
+print("Mehrwertsteuer bei Benzin: 24,74 %");
+nettoPreisGesamt = bruttoPreisGesamt / 1.2474;
+print("Gesamtbetrag Tankfüllung (Netto): ", "%4.4f" % nettoPreisGesamt);

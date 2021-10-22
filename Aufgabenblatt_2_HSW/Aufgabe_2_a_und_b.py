d5d9ccfc6eeaf00febf78d5a836176bab4c26af3
louiz7007 <louis.dittmann@web.de>
2021-10-22 13:12:05 +0200

    Aufgabe 2 fertig

--- Aufgabenblatt_2_HSW/Aufgabe_2_a_und_b.py
+++ Aufgabenblatt_2_HSW/Aufgabe_2_a_und_b.py
@@ -1,14 +1,18 @@
 # Aufgabe 2: Berechnung Volumen Pyramide in Ellen
 # Umrechnung in Kubikmeter
-# Umrechnung in römische Scheffel
+# Umrechnung in römische Scheffel ! nicht gefunden !
 # Ausgabe beider Ergebnisse
 # Zusätzlich: Ausgabe benötigter Sandsteinblöcke
 
 grundflaeche = 440 * 440;
 hoehe = 280;
 volumenEllen = (1/3) * grundflaeche * hoehe;
-print("Das Volumen beträgt ", volumenEllen, " Ellen.");
+print("Das Volumen beträgt ", "%10.2f" % volumenEllen, " Ellen.");
 grundFlaecheMeter = (440 * 0.52) * (440 * 0.52);
 hoeheMeter = 280 * 0.52;
 volumenMeter = (1/3) * grundFlaecheMeter * hoeheMeter;
-print("Das Volumen beträgt ", volumenMeter, " Kubikmeter.");
+print("Das Volumen beträgt ", "%10.2f" % volumenMeter, " Kubikmeter.");
+volumenBlock = 2 * 2 * 2;
+anzahlBenoetigterBloecke = volumenEllen / volumenBlock;
+print("Das entspricht ", "%10.2f" % anzahlBenoetigterBloecke, " Steinblöcke!");
+print("Hinweis: Alle Ergebnisse wurden auf zwei Nachkommastellen gekürzt.");

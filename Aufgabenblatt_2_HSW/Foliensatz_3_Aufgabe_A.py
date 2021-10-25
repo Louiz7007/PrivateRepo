145d170d5984f4cc2880f8ed781b7fe05822244b
Louiz7007 <louis.dittmann@web.de>
2021-10-25 11:02:21 +0200

    Addes Files

---
+++ Aufgabenblatt_2_HSW/Foliensatz_3_Aufgabe_A.py
@@ -0,0 +1,22 @@
+gewicht = float(input("Gewicht in Kg: "))
+groesse = float(input("Größe in m: "))
+print()
+bmx = gewicht / (groesse ** 2)
+
+while (bmx > 25):
+    print("Ihr BMI: ", "%2.2f" % bmx)
+    print()
+    antwort = input("Möchten Sie Ihr Gewicht oder Ihre Größe ändern? (gew / gr)")
+    print()
+    if (antwort == "gew"):
+        gewicht = float(input("Neues Gewicht in Kg: "))
+        print()
+    elif (antwort == "gr"):
+        groesse = float(input("Neue Größe in m: "))
+        print()
+    else:
+        print("Falsche Eingabe! Bitte nur 'gew' oder 'gr' eingeben!")
+    bmx = gewicht / (groesse ** 2)
+print()
+print("Glückwunsch, jetzt leben Sie gesund!")
+print("Ihr BMI: ", "%2.2f" % bmx)

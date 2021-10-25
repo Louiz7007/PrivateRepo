9f8e68f873ff2b73803a850e9c23c07e8f738c61
Louiz7007 <louis.dittmann@web.de>
2021-10-25 18:54:52 +0200

    improeved

--- Aufgabenblatt_3_HSW/Aufgabe_2.py
+++ Aufgabenblatt_3_HSW/Aufgabe_2.py
@@ -1,93 +1,70 @@
 import random
 
 """
-Gibt die Spielerklärung auf der Konsole wieder
-"""
-def startMessage():
-    print("""
-    42 + 7 - Spiel""")
-    print("""
-    Du und der Computer wählen nacheinander eine Zahl von
-    1 bis 16. Die Zahlen werden aufeinander addiert. Der Spieler,
-    der die 50 überschreitet, hat verloren. Durch Eingabe einer '0'
-    beendest du das Spiel""")
-
-"""
-Funktion für den Zug des Spielers
+Funktion für den Spielerzug
 """
 def playerTurn():
-    number = int(input("Du: "))
-    return number
+    return int(input("Du: "))
 
 """
-Funktion für den Zug des Computers
+Funktion für den Computerzug
 """
 def computerTurn():
-    number = random.randint(1, 16)
-    print("Computer: ", number)
-    return number
+    return random.randint(1, 16)
 
 """
-Funktion einer Spielrunde
+Funktion für den Spielablauf
 """
 def game():
     points = 0
-    correct = False
-    while(42):
-        while(not correct):
+    weiter = True
+    while(weiter == True):
+        """
+        Unendliche While-Schleife. Sie wird nur durch breaks durchbrochen
+        """
+        correct = False
+        while(correct != True):
+            """
+            Prüft, ob die Eingabe des Spielers korrekt ist
+            """
             playerNumber = playerTurn()
             if (playerNumber == 0):
-                print("Du hast das Spiel beendet!")
-                end = True;
-            elif (playerNumber >= 1 or playerNumber <= 16):
+                print("Spiel beendet!")
+                break;
+            elif (playerNumber < 0 or playerNumber > 16):
+                print("Falsche Eingabe!")
+                correct = False
+            else:
                 correct = True
+            
         points += playerNumber
-        correct = False
-        if (points >= 50):
+
+        if (points >=50):
+            print("Summe: ", points)
             print("Du hast verloren!")
             break;
-        print("Summe: ", points)
-        print()
 
-        points += computerTurn()
+        print("Summe: ", points)
+        
+        computerNumber = computerTurn()
+        
+        print("Computer: ", computerNumber)
+
+        points += computerNumber
+
         if (points >= 50):
+            print("Summe: ", points)
             print("Computer hat verloren!")
             break;
+
         print("Summe: ", points)
         print()
-        
+
 """
-Hier beginnt das Spiel
+Hier findet das Spiel statt
 """
-startMessage()
-print()
-game()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+i = 0
+anzahlRunden = int(input("Anzahl Runden: "))
+while(i < anzahlRunden):
+    game()
+    i += 1
